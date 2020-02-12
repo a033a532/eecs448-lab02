40ae35c46556920f2c5745ae33b614db025e7816
Adam Auer <adamauer1@ku.edu>
2020-02-12 09:26:56 -0600

    Fixed the search method

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -40,7 +40,14 @@ bool LinkedList<T>::search(T value) const
 {
 	Node<T>* temp = m_front;
 	bool isFound = false;
-
+  while(temp != nullptr)
+	{
+		if (temp->getValue() == value)
+		{
+			isFound = true;
+		}
+		temp= temp->getNext();
+	}
 	/** TODO
 		Fix this method
 	*/
